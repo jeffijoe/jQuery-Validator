43dcaecd9ac97cd20be1ab942a5cdeafafc90707
Jeff Hansen <jeffijoe@hotmail.com>
2013-05-13 16:02:50 +0200

    Added Select Validator.

---
+++ Src/Plugins/jQuery.Validator.Select.js
@@ -0,0 +1,23 @@
+﻿/*
+	File: jQuery.Validator.Select.js
+	Version: 0.2 (jQuery.Validator 1.4.6)
+	Author: Jeff Hansen (Jeffijoe) - Livesys.com
+	jQuery: Tested with v1.9.1
+	Description: Selectbox Validation
+
+    Regex author: Diego Perini
+*/
+jQuery(function () {
+    // Extend the validator object.
+    $.Validator.Extend({
+        dataProp: "required", // The property on the element
+        configProp: "required", // The property on the config.
+        messageDataProp: "msg_empty", // The error message property on the element
+        messageConfigProp: "msg_empty", // The error message property on the config
+        method: function (paramObj) {
+            
+            // Success, return true!
+            return true;
+        }
+    });
+});
