c440fcb1de5c61fc2a223aa46e1f57fe07f9f68f
Jeff Hansen <jeffijoe@hotmail.com>
2013-08-23 10:02:33 +0200

    Added Culture validator. Made Email plugin case-insensitive.

--- Src/Plugins/jQuery.Validator.CultureCode.js
+++ Src/Plugins/jQuery.Validator.CultureCode.js
@@ -1,8 +1,8 @@
 /*
-	File: jQuery.Validator.EmailPlugin.js
+	File: jQuery.Validator.CultureCode.js
 	Version: 0.8 (jQuery.Validator 1.4.7)
 	Author: Jeff Hansen (Jeffijoe) - Livesys.com
-	jQuery: Tested with v1.8.2
+	jQuery: Tested with v2.0
 	Description: E-Mail validation plugin for jQuery.Validator.js
 */
 jQuery(function () {
@@ -11,17 +11,17 @@ jQuery(function () {
 
 	// Extend the validator object.
 	$.Validator.Extend({
-		dataProp: "email", // The property on the element - eg. <input type="text" data-email="true" />
-		configProp: "isEmail", // The property on the config.
-		messageDataProp: "msg_invalidemail", // The error message property on the element
-		messageConfigProp: "msg_invalidemail", // The error message property on the config
+		dataProp: "culturecode", // The property on the element - eg. <input type="text" data-email="true" />
+		configProp: "isCultureCode", // The property on the config.
+		messageDataProp: "msg_invalidculturecode", // The error message property on the element
+		messageConfigProp: "msg_invalidculturecode", // The error message property on the config
 		defaultErrorMessage: _defaultErrorMessage, // Default error message when validation fails and no message has been explicitly set.
 		method: function (paramObj) {
 			// The property value indicates if this is an email field or not.
 			// Obviously, if it wasnt, the property wouldnt be there, but oh well. :P
 			if (paramObj.propertyValue)
 				// E-Mail regex check
-				if (!new RegExp(/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/i).test(paramObj.input.val())) {
+			    if (!new RegExp(/^[a-z]{2,3}(?:-[A-Z]{2,3}(?:-[a-zA-Z]{4})?)?$/).test(paramObj.input.val())) {
 					return false;
 				}
 			// Success, return true!
